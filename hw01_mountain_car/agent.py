985276bbb9e2120cf059aaedf3071bcca12e8486
Nikita Sokolov <faucct@gmail.com>
2020-01-26 09:54:13 +0300

    Preinitialize weights properly

--- hw01_mountain_car/agent.py
+++ hw01_mountain_car/agent.py
@@ -1,15 +1,18 @@
 import random
 import numpy as np
 import os
+import torch
 from .train import transform_state
 
 
 class Agent:
     def __init__(self):
-        self.weight, self.bias = np.load(__file__[:-8] + "/agent.npz")
-        
+        self.Q = torch.nn.Linear(7, 3)
+        with torch.no_grad():
+            self.Q.weight[:], self.Q.bias[:] = np.load(__file__[:-8] + "/agent.npz")
+
     def act(self, state):
-        return np.argmax(self.weight.dot(transform_state(state)) + self.bias)
+        return np.argmax(self.Q(transform_state(state)).detach().numpy())
 
     def reset(self):
         pass
--- hw01_mountain_car/train.py
+++ hw01_mountain_car/train.py
@@ -4,6 +4,7 @@ import torch
 import copy
 from collections import deque
 import random
+from tqdm import tqdm
 
 N_STEP = 3
 GAMMA = 0.96
@@ -11,7 +12,7 @@ GAMMA = 0.96
 
 def transform_state(state):
     state = np.array(state)
-    state = np.concatenate((state + np.array((1.2, 0.0)) / np.array((1.8, 0.07)), np.sin(state), np.cos(state)))
+    state = np.concatenate(((np.sign(state[1]),), state + np.array((1.2, 0.0)) / np.array((1.8, 0.07)), np.sin(state), np.cos(state)))
     result = []
     result.extend(state)
     return np.array(result)
@@ -20,23 +21,20 @@ def transform_state(state):
 class AQL:
     def __init__(self, state_dim, action_dim):
         self.gamma = GAMMA ** N_STEP
-        self.Q = torch.nn.Linear(6, 3)
+        self.Q = torch.nn.Linear(7, 3)
         with torch.no_grad():
-            self.Q.weight[:] = torch.zeros((3, 6))
-            self.Q.weight[0, 1] = -100
-            self.Q.weight[2, 1] = 100
+            self.Q.weight[:] = torch.zeros((3, 7))
+            self.Q.weight[0, 0] = -1
+            self.Q.weight[2, 0] = 1
             self.Q.bias[:] = 0
 
     def act(self, state, target=False):
-        return max(
-            (action for action in range(3)),
-            key=lambda action: self.Q(torch.FloatTensor(state))[action],
-        )
+        return np.argmax(self.Q(torch.FloatTensor(state)).detach().numpy())
 
     def update(self, transition):
         state, action, next_state, reward, done = transition
-        L2 = 0.99
-        alpha = 0.01
+        L2 = 0.999
+        alpha = 0.0001
         *d_weight, d_bias = alpha * torch.FloatTensor(np.concatenate((state, (1,)))) * (self.Q(torch.FloatTensor(next_state))[action] - reward)
         with torch.no_grad():
             self.Q.weight[action, :] += torch.FloatTensor(d_weight)
@@ -46,8 +44,7 @@ class AQL:
 
     def save(self, path):
         with torch.no_grad():
-            print(self.Q.weight, self.Q.bias)
-            # np.savez("agent.npz", np.array(self.Q.weight), np.array(self.Q.bias))
+            np.savez("agent.npz", self.Q.weight.data.numpy(), self.Q.bias.data.numpy())
 
 
 if __name__ == "__main__":
@@ -56,14 +53,14 @@ if __name__ == "__main__":
     eps = 0.1
     episodes = 2000
 
-    for i in range(episodes):
+    for i in tqdm(range(episodes)):
         state = transform_state(env.reset())
         total_reward = 0
         steps = 0
         done = False
-        reward_buffer = deque(maxlen=N_STEP)
-        state_buffer = deque(maxlen=N_STEP)
-        action_buffer = deque(maxlen=N_STEP)
+        reward_buffer = deque()
+        state_buffer = deque()
+        action_buffer = deque()
         while not done:
             if random.random() < eps:
                 action = env.action_space.sample()
@@ -79,10 +76,9 @@ if __name__ == "__main__":
             if len(reward_buffer) == N_STEP:
                 aql.update((state_buffer[0], action_buffer[0], next_state, sum([(GAMMA ** i) * r for i, r in enumerate(reward_buffer)]), done))
             state = next_state
-            env.render()
         if len(reward_buffer) == N_STEP:
             rb = list(reward_buffer)
             for k in range(1, N_STEP):
                 aql.update((state_buffer[k], action_buffer[k], next_state, sum([(GAMMA ** i) * r for i, r in enumerate(rb[k:])]), done))
-        if i % 1 == 0:
+        if i % 20 == 0:
             aql.save('foo')
